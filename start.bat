6731a5cfe25bcb07cb0bd6f5df66fcd0e13b5177
sebl <sebl@schnellebuntebilder.de>
2013-02-11 14:20:03 +0100

    some onSite adjustments

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-start C:\apps\code\vvvv_45beta29_x86\vvvv.exe /dx9ex /o "c:\dev\vvvv\Foosball3000\Setup.v4p"
+start C:\vvvv\vvvv_45beta29_x86\vvvv.exe /dx9ex /o "C:\vvvv\Foosball3000\Setup.v4p"
